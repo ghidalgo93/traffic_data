8cbc449b60049f743ef0b35bdbe6a824294d4e7c
ghidalgo93 <ghidalgo93@gmail.com>
2017-12-21 13:45:20 -0700

    don

--- pycode/traffic_data_cleanup.py
+++ pycode/traffic_data_cleanup.py
@@ -4,11 +4,15 @@
 
 import csv
 
-infile = csv.reader(open('Traffic_Violations.csv', 'r'))
-outfile = csv.writer(open('SpeedExceeded.csv', 'w'), delimiter = ',')
+#importing data and making a new data csv file
+infile = csv.reader(open('../data/Traffic_Violations.csv', 'r'))
+outfile = csv.writer(open('../data/SpeedExceeded.csv', 'w'), delimiter = ',')
 
-
+#writing header from old data and applying it to new data
 header = next(infile)
+#adding some columns to new data
+header.append('Time.Of.Day_decimal')
+header.append('DayOfMonth')
 header.append('SpeedLimit')
 header.append('SpeedCaught')
 header.append('SpeedOver')
@@ -29,6 +33,10 @@ for row in infile:
         if len(intsfound) == 2:
             over = abs(intsfound[0] - intsfound[1])
             if over < 200 and min(intsfound)%5 == 0:
+                ts = row[1].split(":") #ts is time stamp
+                row.append(float(ts[0])+float(ts[1])/60+float(ts[2])/3600)
+                date = row[0].split("/")
+                row.append(date[1])
                 row.append(min(intsfound))
                 row.append(max(intsfound))
                 row.append(over)
--- rcode/traffic_analysis.R
+++ rcode/traffic_analysis.R
@@ -2,38 +2,51 @@
 #November 30, 2017
 #Looking at some traffic data
 
-#sets the working directory 
-setwd("~/Dropbox/traffic_data")
+###Libraries
+library(ggplot2)
 
-#importing data
-Traffic_Violations <- read.csv("~/Dropbox/traffic_data/SpeedExceeded.csv")
+####sets the working directory 
+setwd("~/Dropbox/traffic_data/rcode")
+
+###importing data
+SpeedExceeded <- read.csv("~/Dropbox/traffic_data/data/SpeedExceeded.csv")
 
 
-##Histogram with a normal curve
-x <- SpeedExceeded$SpeedOver
-h <- hist(x, breaks = 100, xlim = c(0,50), col = "red", xlab = "Speed Over Speed Limit", main = "Histogram with Normal Curve")
-xfit<-seq(min(x),max(x),length=40) 
-yfit<-dnorm(xfit,mean=mean(x),sd=sd(x)) 
-yfit <- yfit*diff(h$mids[1:2])*length(x) 
-lines(xfit, yfit, col="blue", lwd=2)
+###Histogram with a normal curve (freq v. speedover)
+#x <- SpeedExceeded$SpeedOver
+#h <- hist(x, breaks = 100, xlim = c(0,50), col = "red", xlab = "Speed Over Speed Limit", main = "Histogram with Normal Curve")
+#xfit<-seq(min(x),max(x),length=40) 
+#yfit<-dnorm(xfit,mean=mean(x),sd=sd(x)) 
+#yfit <- yfit*diff(h$mids[1:2])*length(x) 
+#lines(xfit, yfit, col="blue", lwd=2)
+###same hist, but with ggplot 2, much better graphics
+#data=data.frame(value=SpeedExceeded$SpeedOver)
+#ggplot(data, aes(x=value)) + geom_histogram(binwidth = 1, color="white", fill=rgb(0.2,0.7,0.1,0.4))
 
-##Kernel Density Plot
+###Kernel Density Plot (freq v. speedover)
 #d <- density(Traffic_Violations_SpeedExceeded$SpeedOver) # returns the density data 
 #plot(d, xlim = c(0, 100)) # plots the results
 
-##comparing speed over for hwy and no-hwy roads
+###comparing speed over for hwy and no-hwy roads (freq v. speedover)
 #library(sm)
 #attach(SpeedExceeded)
 # plot densities 
 #sm.density.compare(SpeedExceeded$SpeedOver, Hwy, xlab="Speed Over")
 #title(main="Speed Over Distribution by Hwy or Normal Road")
-# add legend via mouse click
+##add legend via mouse click
 #colfill<-c(2:(2+length(levels(Hwy)))) 
 #legend(locator(1), levels(Hwy), fill=colfill)
 
 
-
-
-##Histogram of Frequency of 9mph over at differnent speed limits
+###Histogram of Frequency of 9mph over at differnent speed limits
 #hist(SpeedExceeded[SpeedExceeded$SpeedOver == 9,]['SpeedLimit']$SpeedLimit, breaks=100, col="red", xlab = 'Speed Limit (mph)', main = 'Frequency of 9mph over at different Speed Limits')
+
+###Histogram (freq v. time of day)
+#hist(SpeedExceeded$Time.Of.Day_decimal , breaks = 24, col="red", xlab = 'Time of Day', main = 'Freq v. Time of Day')
+
+##Histogram (freq v. day of the month)
+#day <- SpeedExceeded$DayOfMonth
+#hist(day , breaks = 31, col="red", xlab = 'Day of the Month', main = 'Freq v. Day of the Month')
+
+
 
